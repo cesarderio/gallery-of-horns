d8c0c8e69040a1a8c40c96c10cdd71350003dc78
Raphael Chookagian <raphaelchookagian@gmail.com>
2022-10-18 17:36:19 -0700

    checks

--- src/Person.js
+++ src/Person.js
@@ -14,3 +14,19 @@ class Person extends React.Component{
 }
 
 export default Person;
+
+
+
+class Person extends React.Component{
+  render(){
+    return(
+      <article>
+        <h3>{this.props.name}</h3>
+        <p>{this.props.course}</p>
+      </article>
+
+    )
+  }
+}
+
+export default Person;
